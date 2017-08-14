4886bccc1f7e2b2182c14cd7057b6e1f86c0168c
houthacker <houthacker@houthacker.io>
2017-08-14 04:54:27 +0200

    Initial commit

---
+++ src/main/java/io/houthacker/notifications/NotificationsApplication.java
@@ -0,0 +1,14 @@
+package io.houthacker.notifications;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Environment;
+
+/**
+ * @author houthacker
+ */
+public class NotificationsApplication extends Application<NotificationsConfiguration> {
+    
+    public void run(NotificationsConfiguration notificationsConfiguration, Environment environment) throws Exception {
+
+    }
+}
